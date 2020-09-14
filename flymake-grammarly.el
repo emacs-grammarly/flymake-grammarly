ed349aa72964a9421801d0d406f0e604149919ae
JenChieh <jcs090218@gmail.com>
2020-09-14 10:29:44 +0800

    Bump version.

--- flymake-grammarly.el
+++ flymake-grammarly.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Flymake support for Grammarly.
 ;; Keyword: grammar check
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "26.1") (grammarly "0.1.0"))
 ;; URL: https://github.com/jcs-elpa/flymake-grammarly
 
